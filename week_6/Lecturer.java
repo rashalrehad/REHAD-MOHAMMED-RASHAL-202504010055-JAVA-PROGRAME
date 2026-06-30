1255a00262dc60cfc54552a24bd3d9845267bc3e
rashalrehad <rashalrehad@gmail.com>
2026-06-30 05:22:07 +0000

    Week six  tutorial

---
+++ week_6/Lecturer.java
@@ -0,0 +1,20 @@
+package week_6;
+public class Lecturer extends Employee {
+
+    private String subject;
+    private String department;
+
+    public Lecturer(String id, String name, String subject, String department){
+        super(id, name);
+        this.subject = subject;
+        this.department = department;
+    }
+
+    public void displaySubject(){
+        System.out.println("Subject : " + subject);
+    }
+
+    public void displayDepartment(){
+        System.out.println("Department  : " + department);
+    }
+}
---
+++ week_6/Main.java
@@ -0,0 +1,9 @@
+package week_6;
+public class Main {
+    public static void main(String[] args) {
+        Lecturer dr = new Lecturer("L200", "Dr Rehad", "Java Programming", "Faculty of Information Technology");
+        dr.displayInfo();
+        dr.displaySubject();
+        dr.displayDepartment();
+    }
+}
