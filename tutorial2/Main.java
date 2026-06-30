a543b8addf2c713178462f88cafe0b4047f99a61
rashalrehad <rashalrehad@gmail.com>
2026-06-30 05:01:50 +0000

    Week two tutorial

---
+++ tutorial2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // coding starts here
+        Student s1 = new Student("REHAD", 23, 3.5);
+        s1.displayInfo();
+        s1.study();
+        s1.displayInfo();
+    }
+}
