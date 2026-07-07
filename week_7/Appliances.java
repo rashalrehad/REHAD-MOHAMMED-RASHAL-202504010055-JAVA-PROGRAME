ea7355bac89809dba9bf76c00417164741f08a8d
rashalrehad <rashalrehad@gmail.com>
2026-07-07 04:31:27 +0000

    Week_7 tutorial7

--- week_7/Appliances.java
+++ week_7/Appliances.java
@@ -1,79 +1,23 @@
 public abstract class Appliances {
-
     protected String brand;
 
     public Appliances(String brand) {
         this.brand = brand;
     }
 
-    public void displayBrand() {
-        System.out.println("Brand : " + brand);
-    }
+    public void turnOn()  { System.out.println("Power ON");  }
+    public void turnOff() { System.out.println("Power OFF"); }
+    public void displayBrand() { System.out.println("Brand : " + brand); }
 
-    public void turnOn() {
-        System.out.println("Power ON");
-    }
-
-    public void turnOff() {
-        System.out.println("Power OFF");
-    }
-
-    // Abstract method - each appliance must define its own operate behaviour
     public abstract void operate();
+}
 
+class WashingMachine extends Appliances {
+    public WashingMachine(String brand) { super(brand); }
+    public void operate() { System.out.println("Washing clothes..."); }
+}
 
-    // ── Concrete class 1: Washing Machine ──────────────────────────────────
-    static class WashingMachine extends Appliances {
-
-        public WashingMachine(String brand) {
-            super(brand);
-        }
-
-        @Override
-        public void operate() {
-            System.out.println("Washing clothes...");
-        }
-    }
-
-
-    // ── Concrete class 2: Refrigerator ─────────────────────────────────────
-    static class Refrigerator extends Appliances {
-
-        public Refrigerator(String brand) {
-            super(brand);
-        }
-
-        @Override
-        public void operate() {
-            System.out.println("Store food & beverages...");
-        }
-    }
-
-
-    // ── Extra class 3: Air Conditioner ─────────────────────────────────────
-    static class AirConditioner extends Appliances {
-
-        public AirConditioner(String brand) {
-            super(brand);
-        }
-
-        @Override
-        public void operate() {
-            System.out.println("Cooling the room...");
-        }
-    }
-
-
-    // ── Extra class 4: Microwave ───────────────────────────────────────────
-    static class Microwave extends Appliances {
-
-        public Microwave(String brand) {
-            super(brand);
-        }
-
-        @Override
-        public void operate() {
-            System.out.println("Heating food...");
-        }
-    }
+class Refrigerator extends Appliances {
+    public Refrigerator(String brand) { super(brand); }
+    public void operate() { System.out.println("Store food & beverages..."); }
 }
--- week_7/Main.java
+++ week_7/Main.java
@@ -1,39 +1,18 @@
 public class Main {
-
     public static void main(String[] args) {
 
-        // Appliance 1 - Washing Machine (LG)
-        Appliances washer = new Appliances.WashingMachine("LG");
-        washer.displayBrand();
-        washer.turnOn();
-        washer.operate();
-        washer.turnOff();
+        WashingMachine w = new WashingMachine("LG");
+        w.displayBrand();
+        w.turnOn();
+        w.operate();
+        w.turnOff();
 
         System.out.println();
 
-        // Appliance 2 - Refrigerator (Panasonic)
-        Appliances fridge = new Appliances.Refrigerator("Panasonic");
-        fridge.displayBrand();
-        fridge.turnOn();
-        fridge.operate();
-        fridge.turnOff();
-
-        System.out.println();
-
-        // Extra Appliance 3 - Air Conditioner (Daikin)
-        Appliances ac = new Appliances.AirConditioner("Daikin");
-        ac.displayBrand();
-        ac.turnOn();
-        ac.operate();
-        ac.turnOff();
-
-        System.out.println();
-
-        // Extra Appliance 4 - Microwave (Samsung)
-        Appliances microwave = new Appliances.Microwave("Samsung");
-        microwave.displayBrand();
-        microwave.turnOn();
-        microwave.operate();
-        microwave.turnOff();
+        Refrigerator r = new Refrigerator("Panasonic");
+        r.displayBrand();
+        r.turnOn();
+        r.operate();
+        r.turnOff();
     }
 }
